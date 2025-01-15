1cb308668246fef92849c156adc65547d2d235ad
salmanshahid5 <salmanshahid0333@gmail.com>
2025-01-15 19:05:27 +0500

    project almost completed

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import userRoute from "./routes/user.js";
+import productRoute from "./routes/product.js";
+import cartRoute from "./routes/cart.js";
+import orderRoute from "./routes/order.js";
+import cors from 'cors';
+import { stripeRoute }  from "./routes/stripe.js"
+
+dotenv.config();
+app.use(express.json());;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(cors());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
+
+// Fallback Route
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Backend server is running on port ${PORT}!`);
+});
